7c4e3e1c48f48689a8aa08d1f96222c2c159699f
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-20 20:27:39 +0900

    Create the Quiz Data

---
+++ src/QuizData.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const QuizData = () => {
+
+    const quizData = [{
+        question: "What is the capital of France?",
+    options: ["London", "Berlin", "Paris", "Madrid"],
+    correctAnswer: "Paris",
+        
+    }]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QuizData
