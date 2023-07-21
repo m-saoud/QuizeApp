548790284bc6e26b157847320dfb5015a0d5ab87
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-21 18:42:39 +0900

    check if the user's answer is correct

--- src/componets/Quiz.js
+++ src/componets/Quiz.js
@@ -1,22 +1,28 @@
-import React, { useState } from 'react'
-import QuizData from '../QuizData'
+import React, { useState } from "react";
+import QuizData from "../QuizData";
 
 const Quiz = () => {
-    const [userAnswers, setUserAnswers] = useState([]);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
+  // Function to handle form submission when the user selects an option
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const userAnswer = formData.get("option");
+      setUserAnswers([...userAnswers, userAnswer]);
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
 
-    const quize =() => {
-        setquastion(quastion)
+    };
+  // Function to check if the user's answer is correct
+
+    const isAnswerCorrect = () => { 
+
+        const currentQuestion=QuizData[currentQuestionIndex]
     }
 
 
+  return <div></div>;
+};
 
-  return (
-      <div>
-          
-    </div>
-  )
-}
-
-export default Quiz
+export default Quiz;
