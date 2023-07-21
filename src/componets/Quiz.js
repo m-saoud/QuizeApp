9e58d902e00cf2e2d56043f6e7312bb0e8f0c83a
m-saoud <131665242+m-saoud@users.noreply.github.com>
2023-07-21 18:17:13 +0900

    State to keep track of the user's answers and the current question index

---
+++ src/componets/Quiz.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import QuizData from '../QuizData'
+
+const Quiz = () => {
+    const [userAnswers, setUserAnswers] = useState([]);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+
+    const quize =() => {
+        setquastion(quastion)
+    }
+
+
+
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default Quiz
